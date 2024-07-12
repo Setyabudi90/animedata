4cffa53f8bc5dd9a7c233f08687547f28caf64e4
i.setya_b <115447070+Setyabudi90@users.noreply.github.com>
2024-07-12 22:59:03 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata = {
-  title: "Animeh - Lihat dan cari anime favoritmu",
+  title: "Animeh - Lihat dan cari anime favoritmu dengan mudah",
   description: "Animeh adalah popular anime database, cari anime dengan mudah, cepat dan aman tanpa gangguan iklan.", 
   keywords: [
     "anime",
@@ -23,12 +23,11 @@ export const metadata = {
     },
   },
   openGraph: {
-    title: "Animeh - Lihat dan cari anime favorimu", 
+    title: "Animeh - Lihat dan cari anime favorimu dengan mudah", 
     image: "https://i.ibb.co.com/Sn7b3YN/dashboard-profile-logo-transformed.jpg", 
     type: "website",
     url: "https://animedata.vercel.app", 
   },
-};
 };
 
 export default function RootLayout({ children }) {

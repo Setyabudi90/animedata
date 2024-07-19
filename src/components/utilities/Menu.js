4d8acad88682b0442ae6abef14644ea227a98933
i.setya_b <115447070+Setyabudi90@users.noreply.github.com>
2024-07-19 09:58:23 +0700

    Update Menu.js

--- src/components/utilities/Menu.js
+++ src/components/utilities/Menu.js
@@ -1,4 +1,4 @@
-export default function Menu({ title}) {
+export default function Menu({ title }) {
     return (
         <>
          <div className="p-8 bg-[url(https://akamai-origin.myanimelist.net/images/anime/1770/97704l.webp)] overflow-hidden object-cover mt-4 mb-6 mx-4">

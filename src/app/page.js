b96ed13b706c8f5e28c93b406f66f3fe3ff19c9f
i.setya_b <115447070+Setyabudi90@users.noreply.github.com>
2024-07-21 10:09:29 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
 import { AnimeResponse, NestedAnimeResponse, Converter } from "@/libs/api";
 import RekomendedAnime from "@/components/Recomendation";
+import { GoogleTagManager } from "@next/third-parties/google";
 export default async function Home() {
   const anime = await AnimeResponse("top/anime", "limit=10");
   let rekomendedAnime = await NestedAnimeResponse(
@@ -11,7 +12,11 @@ export default async function Home() {
   rekomendedAnime = Converter(rekomendedAnime, 10);
   return (
     <>
-      <section>
+      <GoogleTagManager gtmId="GTM-W4GRZ8HG" />
+      <section
+        data-content={btoa(~~(Math.random() * 10000) + "Content")}
+        id={~~(Math.random() * 10000) + "Popular"}
+      >
         <Header
           title={"Paling Populer"}
           linkHref={"/populer"}
@@ -19,7 +24,8 @@ export default async function Home() {
         />
         <AnimeList api={anime} />
       </section>
-      <section>
+      <section data-content={btoa(~~(Math.random() * 10000) + "RekomendedContent")}
+        id={~~(Math.random() * 10000) + "PopularRekomended"}>
         <Header title={"Rekomended Anime"} />
         <RekomendedAnime api={rekomendedAnime} />
       </section>

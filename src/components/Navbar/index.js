c14bbfe5ece719563357dd662f60ec52686b6a5d
i.setya_b <115447070+Setyabudi90@users.noreply.github.com>
2024-07-21 18:01:25 +0700

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import InputSearch from "./InputSearch";
 
 function Navbar() {
   return (
-    <header className="bg-pink-800 backdrop-blur-md p-4 shadow-xl">
+    <header className="bg-pink-800 backdrop-blur-md p-4 shadow-xl sticky">
       <div className="flex md:flex-row flex-col justify-between md:items-center">
         <Link href="/" className={`font-bold text-2xl text-color-primary`}>
           孤独の影
